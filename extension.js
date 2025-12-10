c86787a62b27479f1e4161b3a219f88f6d2c1a56
raahguu <raahguu@outlook.com>
2025-12-10 19:26:07 +0800

    Now has a toggle to turn tailscale on or off

--- extension.js
+++ extension.js
@@ -21,45 +21,103 @@ function myError(string) {
   console.log("Error [tailscale-manager]: " + string);
 }
 
-function SetTailscaleStatus() {
-  try {
-    let proc = Gio.Subprocess.new(
+const TailscaleControler = {
+  // Wrapper to handle all of the command line calls to control tailscale
+  Control: function (command, func) {
+    try {
+      // Create an object that will run the command
+      let proc = Gio.Subprocess.new(
+        command,
+        Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+      );
+
+      // Run the command asynchronously and pass in the function to call when we get it back
+      proc.communicate_utf8_async(null, null, (proc, res) => {
+        // get the output of the function
+        let [, stdout, stderr] = proc.communicate_utf8_finish(res);
+
+        // if it threw an error
+        if (!proc.get_successful()) {
+          myError(stderr);
+          return 0;
+        }
+
+        // otherwise called the passed in function
+        func(stdout);
+        return 1;
+      });
+
+      return true;
+    } catch (e) {
+      myError(e);
+      return false;
+    }
+  },
+
+  // Gets the current tailscale status and then calls setStatusUI to inform it of the new status
+  GetTailscaleStatus: function () {
+    let success = TailscaleControler.Control(
       ["tailscale", "status", "--json"],
-      Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+      (stdout) => {
+        let jsonData = JSON.parse(stdout);
+        tailscale_manager.setStatusUI(jsonData["BackendState"] == "Running");
+      },
     );
 
-    proc.communicate_utf8_async(null, null, (proc, res) => {
-      let [, stdout, stderr] = proc.communicate_utf8_finish(res);
-      if (!proc.get_successful()) {
-        myError(stderr);
-      }
-      const jsonData = JSON.parse(stdout);
+    // Error check if it worked
+    if (success == false) {
+      myError("GetTailscaleStatus failed");
+    }
+    return success;
+  },
 
-      tailscale_manager.setStatusUI(jsonData["BackendState"] == "Running");
+  // Turn tailscale on or off, then calls GetTailscaleStatus to update the menu and ensure it worked
+  SetTailscaleStatus: function (state) {
+    // make sure state is of the correct type so we don't get undefined behaviour
+    if (typeof state != "boolean") {
+      myError("SetTailscaleStatus did not recieve a boolean");
+      return false;
+    }
+
+    let arg = "";
+    arg = state ? "up" : "down";
+    let success = TailscaleControler.Control(["tailscale", arg], (_) => {
+      TailscaleControler.GetTailscaleStatus();
     });
-  } catch (e) {
-    myError(e);
-    return False;
-  }
-}
 
+    // Error check if it worked
+    if (success == false) {
+      myError("SetTailscaleStatus Failed");
+    }
+    return success;
+  },
+};
+
+// The actual menu item class
 const TailscaleMenu = GObject.registerClass(
   class TailscaleMenu extends PanelMenu.Button {
+    // The initliasation function that gets called when GNOME starts to create the item
     _init(dir_path) {
+      // Define our properties
       this.dir_path = dir_path;
       this.icon = null;
       this.status_item = null;
+      this.nodes_submenu = null;
 
       super._init(0);
-      SetTailscaleStatus();
+      // Get the original status and setup the menu
+      TailscaleControler.GetTailscaleStatus();
 
+      // Call _onButtonClick when the system tray icon is ever clicked
       this.connect("button-press-event", this._onButtonClick.bind(this));
     }
 
+    // Event handler for when the system tray icon is clicked
     _onButtonClick() {
-      SetTailscaleStatus();
+      TailscaleControler.GetTailscaleStatus();
     }
 
+    // Set the icon and the toggle
     setStatusUI(status) {
       let icon_on = Gio.icon_new_for_string(this.dir_path + "/icon-on.svg");
       let icon_off = Gio.icon_new_for_string(this.dir_path + "/icon-off.svg");
@@ -72,6 +130,7 @@ const TailscaleMenu = GObject.registerClass(
         status_string = "On";
       }
 
+      // Set the system tray icon
       if (this.icon) {
         this.icon.gicon = used_icon;
       } else {
@@ -82,11 +141,22 @@ const TailscaleMenu = GObject.registerClass(
         this.add_child(this.icon);
       }
 
+      // Set the status toggle in the menu
       if (this.status_item) {
         this.status_item.label.text = status_string;
+        this.setToggledState(status);
       } else {
-        this.status_item = new PopupMenu.PopupMenuItem(status_string, {
-          reactive: false,
+        this.status_item = new PopupMenu.PopupSwitchMenuItem(
+          status_string,
+          status,
+        );
+        //
+        this.status_item.connect("toggled", () => {
+          if (this.status_item.state) {
+            TailscaleControler.SetTailscaleStatus(true);
+          } else {
+            TailscaleControler.SetTailscaleStatus(false);
+          }
         });
         this.menu.addMenuItem(this.status_item);
       }
